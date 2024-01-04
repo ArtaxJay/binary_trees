51f10ed24dd2fb18984d1d2a78735cf3bf1164d9
ArtaxJay <okjaceauthor@gmail.com>
2024-01-04 09:11:43 +0100

    Add the first file

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - will create binary tree nodes.
+ * @parent: A ptr-arg 2 d parent of d node created.
+ * @value: value of d node created.
+ *
+ * Return: In case of an error - NULL.
+ *         Else - a ptr to d node created.
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->node_val = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
