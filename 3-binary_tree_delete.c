20b9367f3a0ab184177d4b77ab1fece45c8e9a03
ArtaxJay <okjaceauthor@gmail.com>
2024-01-04 10:08:42 +0000

    Modifies 3-*

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,13 +2,15 @@
 
 /**
  * binary_tree_delete - Will delete an entire binary tree.
+ * @tree: d tree to delete.
+ * Return: nothing
  */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
-	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
 	free(tree);
 }
